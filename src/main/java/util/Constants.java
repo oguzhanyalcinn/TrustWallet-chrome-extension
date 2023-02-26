83968c3f9bbe36ebcf5697a77f807c3f98879aa2
oguzhan.yalcin <oguzhan.yalcin@trendyol.com>
2023-02-26 20:57:12 +0100

    remove main from gitignore

---
+++ src/main/java/util/Constants.java
@@ -0,0 +1,15 @@
+package util;
+
+public class Constants {
+
+    public static final String ONBOARDING_PAGE = "chrome-extension://egjidjbpglichdcondbcbdnbeeppgdph/home.html#/onboarding";
+    public static final String PASSWORD_PAGE = "chrome-extension://egjidjbpglichdcondbcbdnbeeppgdph/home.html#/onboarding/create/password";
+    public static final String PHRASE_INITIATE_PAGE = "chrome-extension://egjidjbpglichdcondbcbdnbeeppgdph/home.html#/onboarding/create/seed-phrase-initiate";
+    public static final String PHRASE_PAGE = "chrome-extension://egjidjbpglichdcondbcbdnbeeppgdph/home.html#/onboarding/create/seed-phrase";
+    public static final String VERIFY_PAGE = "chrome-extension://egjidjbpglichdcondbcbdnbeeppgdph/home.html#/onboarding/create/verify";
+    public static final String BEST_PRACTISE_LINK = "https://support.trustwallet.com/en/support/solutions/articles/67000659175-best-practices-for-storing-your-recovery-phrase";
+    public static final String ANALYTICS_PAGE = "chrome-extension://egjidjbpglichdcondbcbdnbeeppgdph/home.html#/onboarding/analytics";
+    public static final String PRIVACY_LINK = "https://trustwallet.com/privacy-policy";
+    public static final String CONFIRM_PAGE = "chrome-extension://egjidjbpglichdcondbcbdnbeeppgdph/home.html#/onboarding/create/confirm";
+    public static final String HOME_PAGE = "chrome-extension://egjidjbpglichdcondbcbdnbeeppgdph/home.html#/";
+}
